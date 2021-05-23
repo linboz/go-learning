ddb2e17c52e4fd3f51850a8293f69c7c1d49462a
benson <zhenglinbo520@gmail.com>
2021-05-23 10:45:03 +0800

    update code

--- AplusB.go
+++ AplusB.go
@@ -1,1 +1,26 @@
+/**
+输入
+
+输入两个数，a,b
+
+输出
+
+输出a+b的值
+
+样例输入
+
+2 3
+
+样例输出
+
+5
+ */
 package main
+
+import "fmt"
+
+func main(){
+	var a, b int
+	fmt.Scanln(&a,&b)
+	fmt.Println(a+b)
+}
--- Fibonacci.go
+++ Fibonacci.go
@@ -1,1 +1,59 @@
-package go_learning
+/*
+描述
+
+无穷数列1，1，2，3，5，8，13，21，34，55...称为Fibonacci数列，它可以递归地定义为 F(n)=1 ...........(n=1或n=2) F(n)=F(n-1)+F(n-2).....(n>2)
+现要你来求第n个斐波纳奇数。（第1个、第二个都为1)
+
+输入
+
+第一行是一个整数m(m<5)表示共有m组测试数据 每次测试数据只有一行，且只有一个整形数n(n<20)
+
+输出
+
+对每组输入n，输出第n个Fibonacci数
+
+样例输入
+
+3 1 3 5
+
+样例输出
+
+1 2 5
+
+ */
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func Fibonacci(n int) int{
+	var result int
+	if(n > 2){
+		result = Fibonacci(n-1) + Fibonacci(n-2)
+		return result
+	}else{
+		return 1
+	}
+}
+func main(){
+	var str string
+	var arr []string
+	var res int
+	inputReader := bufio.NewReader(os.Stdin)
+	str,_ = inputReader.ReadString('\n')
+	arr = strings.Fields(str)
+	fmt.Println(arr)
+	for i,_ := range arr {
+		if(i > 0){
+			res,_ = strconv.Atoi(arr[i])
+			fmt.Printf("%d ",Fibonacci(res))
+		}
+	}
+
+}
+
